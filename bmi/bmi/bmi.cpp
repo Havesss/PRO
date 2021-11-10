8c067f927e0a39f9c54dbc2722b907466f229640
Havesss <93245143+Havesss@users.noreply.github.com>
2021-11-10 09:06:40 +0100

    Add files via upload

---
+++ bmi/bmi/bmi.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    cout << "Zadaj svoju vysku v metroch" << endl;
+    float vyska;
+    cin >> vyska;
+    cout << "Zadaj svoju vahu" << endl;
+    float vaha;
+    cin >> vaha;
+    float bmi;
+    bmi = (vaha / (vyska * vyska) *10000);
+    cout << "Tvoje bmi je " << bmi << endl;
+    if (bmi <= 18.5)
+
+        cout << "Si extremne podvyziveny, mal by si viac jest!" << endl; // Si extremne podvyziveny, mal by si viac jest!
+
+    if (bmi > 18.5 && bmi <= 24.99)
+        cout << "Mas normalnu vahu" << endl; // Mas normalnu vahu
+
+    if (bmi > 25 && bmi <= 29.99)
+        cout << "Mas miernu nadvahu" << endl;  // Máš miernu nadváhu
+
+    if (bmi > 30 && bmi <= 39.99)
+        cout << "Si obezny, mal by si trochu pocvicit a jest menej." << endl; 
+
+    if (bmi > 40)
+        cout << "Si extremne obezny, ihned musis schudnut!" << endl;
+
+
+ 
+    
+}
+
