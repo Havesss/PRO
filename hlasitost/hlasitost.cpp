230e1aca1a24dadc73ec61d94c0837c15a132a57
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-12 08:58:57 +0100

    Add files via upload

---
+++ hlasitost/hlasitost.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const string google = "okgoogle";
+	const string siri = "heysiri";
+    int hlas;
+	hlas = 0;
+    string prikaz;
+	cin >> prikaz;
+    if (prikaz == google || prikaz == siri) {
+    cout << "Zadal si spravny prikaz" << endl;
+	cout << "Zadaj hlasitost" << endl;
+	}
+	else {
+	cout << "zadal si chybny prikaz" << endl;
+	}
+	cin >> hlas;
+	if (hlas <= 100) {
+	cout << "Zvysujem hlasitost na " << hlas << endl;
+	}
+	else {
+	cout << "Chybny prikaz" << endl;
+	}
+}
+
+
