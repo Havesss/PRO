9d34c9debcb6d4b8ec71f859b9149a1973f8ee02
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-12 09:31:45 +0100

    Add files via upload

---
+++ KlopKlopKlopPenny/KlopKlopKlopPenny.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int klop = 0;
+	int penny = 3;
+	do {
+		cout << "Klop!" << endl;
+		klop++;
+	} while (klop < penny);
+	cout << "Penny!" << endl;
+	return 0;
+}
