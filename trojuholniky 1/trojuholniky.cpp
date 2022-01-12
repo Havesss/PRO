aa4d09a1b8e47e06890528b6d1945e15d9c20d6a
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-12 08:36:45 +0100

    Add files via upload

---
+++ trojuholniky 1/trojuholniky.cpp
@@ -0,0 +1,19 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j <= (3 - i); j++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+	return 0;
+		
+}
+
