0181b4d40dfb072ad9e5b514d50959852bd864c0
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-12 08:49:20 +0100

    Add files via upload

---
+++ switch/switch.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Zadaj jedno zo slov - cestoviny , boj , zvucka :" << endl;
+	string slovo;
+	int cislo;
+
+	cin >> slovo;
+	if (slovo == "cestoviny") {
+		cislo = 1;
+	}
+	else
+		if (slovo == "boj") {
+			cislo = 2;
+		}
+		else
+			if (slovo == "zvucka") {
+				cislo = 3;
+			}
+
+	switch (cislo) {
+	case 1:
+		cout << "Som hladny." << endl;
+		break;
+	case 2:
+		cout << "Do zbrane." << endl;
+		break;
+	case 3:
+		cout << "Nananananaaaaa." << endl;
+		break;
+	default:
+		cout << "Toto slovo nepoznam." << endl;
+		break;
+	}
+
+	return 0;
+	
+}
+
+ 
