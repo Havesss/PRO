d1675b694da4aed405a23830925c83bd4fd53d18
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-12 08:53:45 +0100

    Add files via upload

---
+++ if vekk/if vekk.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Zadaj svoj vek:" << endl;
+
+	int vek;
+	cin >> vek;
+
+	if (vek >= 18) 
+	{
+		cout << "si plnolety" << endl;
+
+	}
+	else {
+
+		cout << "nie si plnolety" << endl;
+	}
+}
+
+
+
+
+
+ 
