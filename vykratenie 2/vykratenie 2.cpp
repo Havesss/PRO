79ae7556e7243bc5c03a99b89aad470d1cc15d9d
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-21 01:29:54 +0100

    Add files via upload

---
+++ vykratenie 2/vykratenie 2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Napis cislo" << endl;       //vypytanie cisla od uzivatela
+	int cislo;            //deklaracia premennej
+	cin >> cislo;
+	int vysledok;         //deklaracia premennej
+	vysledok = cislo * 2;  //vyratanie vysledku nasobkom 2
+	cout << "vysledok je " << vysledok << endl;    //vypisanie vysledku od uzivatela
+	return 0;
+}
