4d9647d6b36dcf9ffa1b1717ea57891455a1e10c
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-21 01:29:34 +0100

    Add files via upload

---
+++ Pi/Pi.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{    
+	int a = 10;            // Deklaracia premennej //Datovy typ int
+	cout << "Hodnota a po pretypovani z int na char " << (char)a << endl;      // Vypis je hodnota (a) po pretypovani z int na char:b EXPLICITNE PRETYPOVANIE
+	float d = 10.1;
+	float r;
+	r = d / 2;
+	float O;
+	const double PI = 3.14;                 // Deklaracia a inicializacia konstanty , PI je urcene ako konstanta, cislo sa nemeni.
+    cout << "Obvod kruhu je " << (O = 2 * PI * r) << endl;          // Vypocet obvodu kruhu
+
+	return 0;
+}
