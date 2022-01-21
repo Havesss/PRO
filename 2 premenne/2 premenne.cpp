4766a46cdb7fcae894878d2c938ae0e2e443d528
Havesss <93245143+Havesss@users.noreply.github.com>
2022-01-21 01:29:04 +0100

    Add files via upload

---
+++ 2 premenne/2 premenne.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;             //spojenie a,b dokopy na ulahcenie a skratenie textu                              
+	a = 10;                                                      
+	b = 99;
+	cout << "A= " << a << endl;
+	cout << "B= " << b << endl;     
+	/*konstanta hodnota a.... a konstanta hodnota b sa vypise. 
+	  prikaz cout na vypisanie konstantnej (nemennej) hodnoty a = 99
+	  prikaz cout na vypisanie konstantnej (nemennej) hodnoty b = 10
+	*/
+    cout << "Vymenenie obsahu premennych" << endl;        // vypisanie vymenenia obsahu premennych
+	int aa, bb ;     //spojenie aa,bb dokopy na ulahcenie a skratenie textu          
+	aa = 99;
+	bb = 10;
+	cout << "A= " << aa << endl;
+	cout << "B= " << bb << endl;
+	/*konstanta hodnota a.... a konstanta hodnota b sa vypise.
+	  prikaz cout na vypisanie konstantnej (nemennej) hodnoty aa = 99
+	  prikaz cout na vypisanie konstantnej (nemennej) hodnoty bb = 10
+	*/
+	return 0;
+}
